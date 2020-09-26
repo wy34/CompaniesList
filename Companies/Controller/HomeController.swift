5c479f976fa946a561c10954cb21706579a25050
William Yeung <williamyeung1141998@gmail.com>
2020-09-26 16:11:28 -0400

    Converted to tableView and fixed compact navbar title color

--- Companies/Controller/HomeController.swift
+++ Companies/Controller/HomeController.swift
@@ -7,21 +7,23 @@
 
 import UIKit
 
-class HomeController: UIViewController {
+class HomeController: UITableViewController {
     // MARK: - Properties
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavBar()
+        configureTableView()
     }
 
     // MARK: - Helpers
     func configureNavBar() {
         let newAppearance = UINavigationBarAppearance()
         newAppearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        newAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         newAppearance.backgroundColor = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
-        
+
         navigationController?.navigationBar.scrollEdgeAppearance = newAppearance
         navigationController?.navigationBar.standardAppearance = newAppearance
         navigationController?.navigationBar.compactAppearance = newAppearance
@@ -30,6 +32,11 @@ class HomeController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.isTranslucent = false
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+        tableView.separatorStyle = .none
     }
     
     // MARK: - Selectors
