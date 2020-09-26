ab0a973989b5286853a711d610c5b5efd647c691
William Yeung <williamyeung1141998@gmail.com>
2020-09-26 15:28:49 -0400

    Initial project

---
+++ Companies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Companies
+//
+//  Created by William Yeung on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
