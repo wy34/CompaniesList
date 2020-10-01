bd7dfa0d299cca32ef1c6549942b60c68e291b87
William Yeung <williamyeung1141998@gmail.com>
2020-09-30 23:25:25 -0400

    Add employee button

--- Companies/Controller/EmployeesController.swift
+++ Companies/Controller/EmployeesController.swift
@@ -19,6 +19,17 @@ class EmployeesController: UITableViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
+    }
+    
+    // MARK: - Helpers
+    private func configUI() {
         view.backgroundColor = .darkBlue
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus"), style: .plain, target: self, action: #selector(handleAdd))
+    }
+    
+    // MARK: - Selectors
+    @objc private func handleAdd() {
+        print("Adding employee")
     }
 }
