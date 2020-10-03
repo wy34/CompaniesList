c0f4c0052a9e78b4cb8d2a6190992ba3c3cc674a
William Yeung <williamyeung1141998@gmail.com>
2020-10-03 14:33:45 -0400

    Added employeeTypeSegmentedControll

--- Companies/Controller/CreateEmployeeController.swift
+++ Companies/Controller/CreateEmployeeController.swift
@@ -21,6 +21,12 @@ class CreateEmployeeController: UIViewController {
     private let birthdayLabel = UILabel.createBasicLabel(withText: "Birthday")
     private let birthdayTextfield = UITextField.createBasicTextField(withPlaceholder: "mm/dd/yyyy")
     
+    private let employeeTypeSegmentedControll: UISegmentedControl = {
+        let types = ["1", "2", "3"]
+        let sc = UISegmentedControl(items: types)
+        return sc
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,10 +44,10 @@ class CreateEmployeeController: UIViewController {
     func configUI() {
         view.backgroundColor = .darkBlue
         
-        let nameBackGroundView = setupNameBackgroundView(withHeightMultipleOf: 0.065 * 2)
-        view.addSubviews(nameLabel, nameTextfield, birthdayLabel, birthdayTextfield)
+        let nameBackGroundView = setupNameBackgroundView(withHeightMultipleOf: 0.065 * 3)
+        view.addSubviews(nameLabel, nameTextfield, birthdayLabel, birthdayTextfield, employeeTypeSegmentedControll)
         
-        nameLabel.setDimension(height: nameBackGroundView.heightAnchor, hMult: 0.5)
+        nameLabel.setDimension(height: nameBackGroundView.heightAnchor, hMult: 0.333)
         nameLabel.center(to: nameBackGroundView, by: .centerX, withMultiplierOf: 0.18)
         nameLabel.anchor(top: nameBackGroundView.topAnchor)
         
@@ -49,13 +55,16 @@ class CreateEmployeeController: UIViewController {
         nameTextfield.setDimension(width: nameBackGroundView.widthAnchor, wMult: 0.68)
         nameTextfield.center(to: nameBackGroundView, by: .centerX, withMultiplierOf: 1.25)
         
-        birthdayLabel.setDimension(height: nameBackGroundView.heightAnchor, hMult: 0.5)
+        birthdayLabel.setDimension(height: nameBackGroundView.heightAnchor, hMult: 0.333)
         birthdayLabel.anchor(left: nameLabel.leftAnchor)
         birthdayLabel.anchor(top: nameLabel.bottomAnchor)
         
         birthdayTextfield.center(to: birthdayLabel, by: .centerY)
         birthdayTextfield.setDimension(width: nameBackGroundView.widthAnchor, wMult: 0.68)
         birthdayTextfield.center(to: nameBackGroundView, by: .centerX, withMultiplierOf: 1.25)
+        
+        employeeTypeSegmentedControll.setDimension(height: nameBackGroundView.heightAnchor, hMult: 0.25)
+        employeeTypeSegmentedControll.anchor(top: birthdayLabel.bottomAnchor, right: birthdayTextfield.rightAnchor, left: birthdayLabel.leftAnchor)
     }
     
     func showErrorAlert(withTitle title: String, andMessage message: String) {
